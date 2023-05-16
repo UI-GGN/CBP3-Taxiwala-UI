81a898eeedaecbf4ab665b429df4e6d48e2e9b37
“ankitchaudharyy-TW” <ankit.chaudhary@thoughtworks.com>
2023-05-17 04:43:56 +0530

    [Ankit] Add. eslint and testing libraries

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,37 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module',
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "indent": ["error", 2],
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "no-unused-vars": "error",
+    "prefer-const": "error",
+    "max-len": ["error", { "code": 80 }],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/prop-types": "warn",
+    "react/jsx-props-no-spreading": "off",
+    "react/react-in-jsx-scope": "error",
+    "react/no-unused-prop-types": "error",
+    "react/no-unknown-property": "error",
+    "react/self-closing-comp": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
 }
+
+
+   
