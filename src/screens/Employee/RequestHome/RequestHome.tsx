25eefc59528c15c476dd989368fc68979c640d0e
vanshkapoortwks <vansh.kapoor@thoughtworks.com>
2023-05-30 19:57:40 +0530

    [Ankit/VanshK] add request forms

--- src/screens/Employee/RequestHome/RequestHome.tsx
+++ src/screens/Employee/RequestHome/RequestHome.tsx
@@ -3,8 +3,12 @@ import WindowLayout from "../../../Components/WindowLayout";
 import logo from "../../../assets/parked_cab.jpg";
 import Box from "@mui/material/Box";
 import { useState } from "react";
-import { Button } from '@mui/material';
+import { Button, Accordion, AccordionSummary, AccordionDetails, Typography, Grid,
+  ToggleButtonGroup, ToggleButton
+ } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TextInput from "../../../Components/TextInput/TextInput";
 import "./RequestHome.css";
 
 const RightWindow = () => {
@@ -14,6 +18,174 @@ const RightWindow = () => {
 const LeftWindow = () => {
   const TotalSteps = 2;
   const [currentstep, setCurrentstep] = useState(0);
+  const [alignment, setAlignment] = React.useState('Ad-Hoc');  
+
+
+  const Step1 = () => {
+    return <>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>Enter your Pickup location</Typography>
+      </AccordionSummary>
+      <AccordionDetails style={{ width: "100%" }}>
+        <div>
+        <Typography>
+        Pickup point is where you will be coming from towards office!
+        </Typography>
+          <TextInput
+            placeholder='Enter your pickup location'
+            type="text"
+            value={""}
+            handleChange={(text: string) => console.log(text)}
+            styles={{
+              width: "100%",
+              height: "49.4px",
+              marginTop: "10px",
+              marginRight: "40px"
+            }}
+          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6} lg={6}>
+                <TextInput
+                placeholder='PIN Code'
+                type="text"
+                value={""}
+                handleChange={(text: string) => console.log(text)}
+                styles={{
+                  width: "100%",
+                  height: "49.4px",
+                  marginTop: "10px",
+                  marginRight: "40px"
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+            <TextInput
+                placeholder='Nearest Landmark'
+                type="text"
+                value={""}
+                handleChange={(text: string) => console.log(text)}
+                styles={{
+                  width: "100%",
+                  height: "49.4px",
+                  marginTop: "10px",
+                  marginRight: "40px"
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+      </AccordionDetails>
+    </Accordion>
+    <br />
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>Enter your Drop location</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+      <div>
+      <Typography>
+        Drop point is where you will be going to after the office!
+        </Typography>
+          <TextInput
+            placeholder='Enter your pickup location'
+            type="text"
+            value={""}
+            handleChange={(text: string) => console.log(text)}
+            styles={{
+              width: "100%",
+              height: "49.4px",
+              marginTop: "10px",
+              marginRight: "40px"
+            }}
+          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6} lg={6}>
+                <TextInput
+                placeholder='PIN Code'
+                type="text"
+                value={""}
+                handleChange={(text: string) => console.log(text)}
+                styles={{
+                  width: "100%",
+                  height: "49.4px",
+                  marginTop: "10px",
+                  marginRight: "40px"
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+            <TextInput
+                placeholder='Nearest Landmark'
+                type="text"
+                value={""}
+                handleChange={(text: string) => console.log(text)}
+                styles={{
+                  width: "100%",
+                  height: "49.4px",
+                  marginTop: "10px",
+                  marginRight: "40px"
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </AccordionDetails>
+    </Accordion>
+    <br />
+  </>
+  };
+
+  const Step2 = () => {
+    
+    const handleChange = (
+      event: React.MouseEvent<HTMLElement>,
+      newAlignment: string,
+    ) => {
+      setAlignment(newAlignment);
+    };
+
+
+    return <>
+      <Typography>
+        On what regularity are you planning to book cab?
+      </Typography>
+      
+      <ToggleButtonGroup
+      color="primary"
+      value={alignment}
+      exclusive
+      onChange={handleChange}
+      aria-label="Platform"
+    >
+      <ToggleButton value="Ad-Hoc">Ad-Hoc</ToggleButton>
+      <ToggleButton value="Regular">Regular</ToggleButton>
+    </ToggleButtonGroup>
+    <br />
+    <br />
+    {alignment=="Ad-Hoc"&&<div>
+      <Typography>
+      Ad-Hoc requests are request on demand basis. These are to be made 1 hour before atleast.
+      </Typography>
+      </div>
+    }
+    {alignment=="Regular"&&<div>
+      <Typography>
+      Regular requests are qhen you plan to come for many days to office. These are to be made 1 day before by 10pm.
+      </Typography>
+      </div>
+    }
+    </>;
+  };
 
 
   return <>
@@ -24,6 +196,8 @@ const LeftWindow = () => {
         <p className="stepsBox">Step {currentstep+1}/{TotalSteps}</p>
       </div>
 
+      {currentstep==0&&<Step1 />}
+      {currentstep==1&&<Step2 />}
 
       <div style={{display: 'flex'}}>
         {
