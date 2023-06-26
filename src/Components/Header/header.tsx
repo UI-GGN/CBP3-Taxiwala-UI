9dea3e2524afc0316fd37f32bf3625433309ffdc
ashokkumarmuthu-tw <ashok.kumar@thoughtworks.com>
2023-06-26 23:50:51 +0530

    [Ashok] Add. Create header component and ProfileIcon

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { Button } from "@mui/material";
-
+import AccountMenu from "./ProfileIcon";
 
 function Thoughtworks() {
   return (
@@ -14,7 +14,7 @@ function Thoughtworks() {
         fontWeight: 400,
         textTransform: "none",
         padding: "5px",
-        color: "#838383",
+        color: "#838383"
       }}
     >
       /thoughtworks
@@ -22,33 +22,36 @@ function Thoughtworks() {
   );
 }
 
-const navItems = ["Profile","All Requests"];
+const navItems = [
+  // eslint-disable-next-line react/jsx-key
+  <AccountMenu/>,
+  "Profile",
+  "All Requests"
+];
 
 const defaultTheme = createTheme();
 
 export default function HeaderBar() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box 
+      <Box
         component="header"
         sx={{
-          pt: 2,
+          pt: 3,
           pb: 1,
-          px: 0,           
+          px: 0,
           color: "#ADADAD",
           position: "fixed",
           width: "100%",
-          // display: { xs: "none", sm: "block"},
-          fontWeight:"700px",
-          margin:"0px",
+          display: { xs: "none", sm: "block"},
+          fontWeight: "700px",
           backgroundColor: (theme) =>
             theme.palette.mode === "light"
               ? theme.palette.grey[100]
-              : theme.palette.grey[800],
+              : theme.palette.grey[800]
         }}
       >
-        <Grid container >
+        <Grid container>
           <Grid item xs={12} md={9} lg={9}>
             <Container>
               <Typography
@@ -58,7 +61,6 @@ export default function HeaderBar() {
                   fontWeight: 700,
                   color: "#595959",
                   justifyContent: "right",
-                  marginTop:"5px",
                 }}
               >
                 TaxiWala
@@ -66,11 +68,26 @@ export default function HeaderBar() {
               </Typography>
             </Container>
           </Grid>
-          <Grid item xs={12} md={3} lg={3} 
-            sx={{marginTop:0, display: { xs: "none", sm: "block" } }}>
-            <Container sx={{ float: "right", color: "#838383", marginTop: 1,borderRight:0 }}>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            lg={3}
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            <Container
+              sx={{
+                float: "right",
+                color: "#838383",
+                pt:0.5,
+                mr:5
+              }}
+            >
               {navItems.map((item) => (
-                <Button key={item} sx={{ color: "#000000" , float:"right", paddingRight:"10%"}}>
+                <Button
+                  key={item}
+                  sx={{ color: "#000000", float: "right", paddingRight: "7%"}}
+                >
                   {item}
                 </Button>
               ))}
