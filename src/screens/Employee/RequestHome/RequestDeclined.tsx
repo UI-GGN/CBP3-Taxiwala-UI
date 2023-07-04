bfd3d96d0a333b5db1dbee01a648355e0bbace39
Nity-a-priya <nityapriya.katta@thoughtworks.com>
2023-07-04 23:16:31 +0530

    modify requests declined and progress pages

--- src/screens/Employee/RequestHome/RequestDeclined.tsx
+++ src/screens/Employee/RequestHome/RequestDeclined.tsx
@@ -4,6 +4,7 @@ import { RightWindow } from "./RequestHome";
 import { Box, Typography, Button } from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from "react-router-dom";
+import '../../../index.css';
 
 const LeftWindow = () => {
   const navigate = useNavigate();
@@ -15,12 +16,12 @@ const LeftWindow = () => {
       Your request could not be fulfilled at the moment.<br/> Please contact admin staff at <b>Phone number: 987654321</b>
       </Typography>
       <br />
-      <Button variant="outlined"
+      <Button variant="outlined" className="actions"
         onClick={() => {
-          navigate("/employee/home");
+          navigate("/employee/allrequests");
         }}
       >
-        <ArrowBackIcon /> Back to Home
+        <ArrowBackIcon />Back to Requests
       </Button>
     </Box>
   </>;
--- src/screens/Employee/RequestHome/RequestProgress.tsx
+++ src/screens/Employee/RequestHome/RequestProgress.tsx
@@ -4,6 +4,7 @@ import { RightWindow } from "./RequestHome";
 import { Box, Typography, Button } from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from "react-router-dom";
+import '../../../index.css';
 
 const LeftWindow = () => {
   const navigate = useNavigate();
@@ -15,12 +16,12 @@ const LeftWindow = () => {
       Your request has been shared with Gurgaon office Admins. They will get in touch with you soon.
       </Typography>
       <br />
-      <Button variant="outlined"
+      <Button variant="outlined" className="actions"
         onClick={() => {
-          navigate("/employee/home");
+          navigate("/employee/allrequests");
         }}
       >
-        <ArrowBackIcon /> Back to Home
+        <ArrowBackIcon /> Back to Requests
       </Button>
       {/* <Typography variant="h3">Request one now</Typography> */}
     </Box>
