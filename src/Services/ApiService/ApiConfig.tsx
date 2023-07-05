fcfaefb76eb6f826328d0ec2babd5a7bea7cc877
ankitchaudharyy-TW <ankit.chaudhary@thoughtworks.com>
2023-07-05 11:55:26 +0530

    [Ankit] Fix. build errors

--- src/Services/ApiService/ApiConfig.tsx
+++ src/Services/ApiService/ApiConfig.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cab-schedule-serverless.vercel.app/api/v1",
+	baseURL: "https://cab-schedule-serverless.vercel.app/api/v1",
 });
 
-api.interceptors.response.use( undefined, (error) => {
-  return Promise.reject(error);
+api.interceptors.response.use(undefined, (error) => {
+	return Promise.reject(error);
 });
--- src/Services/ApiService/ApiUtils.tsx
+++ src/Services/ApiService/ApiUtils.tsx
@@ -2,33 +2,29 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const apiStatus = {
-  loading: "loading",
-  complete: "complete",
-  error: "error"
+	loading: "loading",
+	complete: "complete",
+	error: "error",
 };
 
-export const GetApiEffect = (service) => {
-  const [status, setStatus] = useState(apiStatus.loading);
-  const [data, setData] = useState(null);
+export const GetApiEffect = (service: any) => {
+	const [status, setStatus] = useState(apiStatus.loading);
+	const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const callService = async () => {
-      const response = await service();
-      console.log(response);
-      setData(response);
-    };
+	useEffect(() => {
+		const callService = async () => {
+			const response = await service();
+			console.log(response);
+			setData(response);
+		};
 
-    try{
-      callService();
-      setStatus(apiStatus.complete);
-    } catch(error) {
-      setStatus(apiStatus.error);
-    }
-  }, []);
+		try {
+			callService();
+			setStatus(apiStatus.complete);
+		} catch (error) {
+			setStatus(apiStatus.error);
+		}
+	}, []);
 
-  return [
-    status === apiStatus.loading,
-    status === apiStatus.error,
-    data
-  ];
+	return [status === apiStatus.loading, status === apiStatus.error, data];
 };
